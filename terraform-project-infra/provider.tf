376e7fbebd01253408bb74f0d29728bd15fb1b8a
progee1 <onyedika198@gmail.com>
2025-11-14 19:56:28 +0100

    Update Terraform provider configuration and backend

--- terraform-project-infra/provider.tf
+++ terraform-project-infra/provider.tf
@@ -1,24 +1,16 @@
+
 terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
-
   backend "s3" {
-    bucket         = var.tfstate_bucket_name    # bootstrap this bucket (or create manually then init)
-    key            = "global/${var.project_name}/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = var.tf_lock_table_name
+    bucket         = "godwin-terraform-state-bucket198"
+    key            = "global/godwin/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "godwin-terraform-lock"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
   # Authentication options (choose one):
   # - Environment variables AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY
   # - ~/.aws/credentials profile (set AWS_PROFILE)
