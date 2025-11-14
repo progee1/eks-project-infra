2db1944ef48b3cbde76d7845afa216c9cfaff473
progee1 <onyedika198@gmail.com>
2025-11-14 21:25:59 +0100

    Create main.tf

---
+++ terraform-bootstrap/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.7.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "godwin-terraform-state-bucket198"        # Must match backend bucket in terraform-project-infra/provider.tf
+  acl    = "private"
+
+  versioning { enabled = true }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "terraform-state-godwin"
+    Environment = "ci"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "lock_table" {
+  name         = "godwin-terraform-lock"            # Must match backend dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-locks-godwin"
+  }
+}
