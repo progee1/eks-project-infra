ace2ad80e90a621f617195f1451439713346a716
progee1 <onyedika198@gmail.com>
2025-11-15 07:02:24 +0100

    Update vpc.tf

--- terraform-project-infra/vpc.tf
+++ terraform-project-infra/vpc.tf
@@ -14,3 +14,13 @@ resource "aws_subnet" "eks_subnets" {
     Name = "eks-subnet-${count.index}"
   }
 }
+
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
