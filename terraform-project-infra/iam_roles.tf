3e69854975f38edb54b32320cfb66c19df0c4301
progee1 <onyedika198@gmail.com>
2025-11-16 04:40:24 +0100

    Update iam_roles.tf

--- terraform-project-infra/iam_roles.tf
+++ terraform-project-infra/iam_roles.tf
@@ -1,154 +1,7 @@
-# IAM role for CodeBuild projects
-resource "aws_iam_role" "codebuild_role" {
-  name = "${var.project_name}-codebuild-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "codebuild.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-  tags = { "Owner" = "Godwin" }
-}
+#######################################
+# EKS CLUSTER ROLE
+#######################################
 
-resource "aws_iam_policy" "codebuild_policy" {
-  name = "${var.project_name}-codebuild-policy"
-  description = "Policy for CodeBuild to interact with ECR, S3, EKS, DynamoDB, CloudWatch"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement: [
-      {
-        Effect: "Allow",
-        Action: [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-          "ecr:DescribeRepositories"
-        ],
-        Resource: "*"
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        Resource: [
-          aws_s3_bucket.artifact_bucket.arn,
-          "${aws_s3_bucket.artifact_bucket.arn}/*"
-        ]
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "eks:DescribeCluster"
-        ],
-        Resource: "*"
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:Query",
-          "dynamodb:Scan",
-          "dynamodb:UpdateItem"
-        ],
-        Resource: aws_dynamodb_table.terraform_lock.arn
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource: "*"
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "ssm:GetParameters",
-          "ssm:GetParameter",
-          "ssm:PutParameter"
-        ],
-        Resource: "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codebuild_attach" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = aws_iam_policy.codebuild_policy.arn
-}
-
-# CodePipeline role
-resource "aws_iam_role" "codepipeline_role" {
-  name = "${var.project_name}-codepipeline-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "codepipeline.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-  tags = { "Owner" = "Godwin" }
-}
-
-resource "aws_iam_policy" "codepipeline_policy" {
-  name = "${var.project_name}-codepipeline-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement: [
-      {
-        Effect: "Allow",
-        Action: [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:PutObject"
-        ],
-        Resource: [
-          aws_s3_bucket.artifact_bucket.arn,
-          "${aws_s3_bucket.artifact_bucket.arn}/*"
-        ]
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "codebuild:StartBuild",
-          "codebuild:BatchGetBuilds",
-          "codebuild:BatchGetProjects"
-        ],
-        Resource: "*"
-      },
-      {
-        Effect: "Allow",
-        Action: [
-          "iam:PassRole"
-        ],
-        Resource: [
-          aws_iam_role.codebuild_role.arn
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
-  role       = aws_iam_role.codepipeline_role.name
-  policy_arn = aws_iam_policy.codepipeline_policy.arn
-}
-
-# EKS Cluster Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
@@ -156,15 +9,12 @@ resource "aws_iam_role" "eks_cluster_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = {
-        Service = "eks.amazonaws.com"
-      },
+      Principal = { Service = "eks.amazonaws.com" },
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-# Missing attachment FIX ✔️
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -178,4 +28,161 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
 resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+
+#######################################
+# EKS NODE ROLE (THIS WAS MISSING)
+#######################################
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+#######################################
+# CODEBUILD ROLE
+#######################################
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.project_name}-codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "codebuild.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "codebuild_policy" {
+  name        = "${var.project_name}-codebuild-policy"
+  description = "Policy for CodeBuild"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:*"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          aws_s3_bucket.artifact_bucket.arn,
+          "${aws_s3_bucket.artifact_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "eks:DescribeCluster"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:*"
+        ],
+        Resource = aws_dynamodb_table.tf_lock.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_policy.arn
+}
+
+
+#######################################
+# CODEPIPELINE ROLE
+#######################################
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.project_name}-codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "codepipeline.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "codepipeline_policy" {
+  name = "${var.project_name}-codepipeline-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          aws_s3_bucket.artifact_bucket.arn,
+          "${aws_s3_bucket.artifact_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = "iam:PassRole",
+        Resource = aws_iam_role.codebuild_role.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.codepipeline_policy.arn
 }
